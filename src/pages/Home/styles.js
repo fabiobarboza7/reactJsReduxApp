1745c4555cd1bd34b21735c6602cea635e12bcd9
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-08-04 17:04:46 -0300

    fixing li style mistake

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -13,56 +13,56 @@ export const ProductList = styled.ul`
     background: #fff;
     border-radius: 4px;
     padding: 20px;
-  }
 
-  img {
-    align-self: center;
-    max-width: 250px;
-  }
-
-  > strong {
-    font-size: 16px;
-    line-height: 20px;
-    color: #333;
-    margin-top: 5px;
-  }
-
-  > span {
-    font-size: 21px;
-    font-weight: bold;
-    margin: 5px 0 20px;
-  }
-
-  button {
-    background: #7159c1;
-    color: #fff;
-    border-radius: 4px;
-    overflow: hidden;
-    margin-top: auto;
-
-    display: flex;
-    align-items: center;
-    transition: background 0.2s;
-
-    &:hover {
-      background: ${darken(0.03, '#7159c1')};
+    img {
+      align-self: center;
+      max-width: 250px;
     }
 
-    div {
+    > strong {
+      font-size: 16px;
+      line-height: 20px;
+      color: #333;
+      margin-top: 5px;
+    }
+
+    > span {
+      font-size: 21px;
+      font-weight: bold;
+      margin: 5px 0 20px;
+    }
+
+    button {
+      background: #7159c1;
+      color: #fff;
+      border-radius: 4px;
+      overflow: hidden;
+      margin-top: auto;
+
       display: flex;
       align-items: center;
-      padding: 12px;
-      background: rgba(0, 0, 0, 0.1);
+      transition: background 0.2s;
 
-      svg {
-        margin-right: 5px;
+      &:hover {
+        background: ${darken(0.03, '#7159c1')};
       }
-    }
 
-    span {
-      flex: 1;
-      text-align: center;
-      font-weight: bold;
+      div {
+        display: flex;
+        align-items: center;
+        padding: 12px;
+        background: rgba(0, 0, 0, 0.1);
+
+        svg {
+          margin-right: 5px;
+        }
+      }
+
+      span {
+        flex: 1;
+        text-align: center;
+        font-weight: bold;
+      }
     }
   }
 `;
