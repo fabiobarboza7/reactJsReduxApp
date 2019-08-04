ba33ba5bbaa53762b013aa610e1460ff322e7f12
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-08-04 02:17:01 -0300

    starting working on global.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import './pages/styles/global';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
 
 function App() {
-  return <h1>Hello Redux</h1>;
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
