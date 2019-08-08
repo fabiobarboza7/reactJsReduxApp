15e9f817f71e0c10fb11efec71b392646c950ba5
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-08-08 14:39:08 -0300

    add immet, prevent adding new items for same product..

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -35,7 +35,7 @@ function Cart({ cart }) {
                   <button type="button">
                     <MdRemoveCircleOutline size={20} color="#7159c1" />
                   </button>
-                  <input type="number" readOnly value={1} />
+                  <input type="number" readOnly value={product.amout} />
                   <button type="button">
                     <MdAddCircleOutline size={20} color="#7159c1" />
                   </button>
